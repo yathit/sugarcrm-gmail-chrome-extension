90f789782c6c0f586d1984c536f91d0c7019f407
kyawtun <okisan.2008@gmail.com>
2014-01-06 00:24:57 +0800

    initial

--- js/run-option-page.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by kyawtun on 15/12/13.
- */
-
-
-var page = new OptionPage();
-page.run();
---
+++ js/run-popup.js
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview About this file
+ */
+
+
+var app = new ydn.crm.popup.Popup();
+app.init();
