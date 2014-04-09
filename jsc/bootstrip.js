1ac052504426ddb20a454b2680c2b6e315c7a108
kyawtun <okisan.2008@gmail.com>
2014-04-10 07:22:15 +0800

    rel 0.7.19

--- jsc/bootstrip.js
+++ jsc/bootstrip.js
@@ -3,5 +3,5 @@
  The Closure Authors under the MIT license. All other code is Copyright
  2005-2009 The Closure Authors. All Rights Reserved.
 */
-function b(h){chrome.storage.local.get("ydn-crm-track",function(c){var a=c["ydn-crm-track"];c="jsc/ydn.crm-0.7.18.js";navigator.onLine&&"Edge"==a?c="https://ydn-src-1.storage.googleapis.com/jsc/ydn.crm-edge.js":"Beta"==a?c="jsc/ydn.crm-0.7.18.js":"RC"==a&&(c="jsc/ydn.crm-0.7.18.js");a=document.createElement("script");a.onload=h;a.type="text/javascript";var g={};g["ydn-crm-src"]=c;chrome.storage.local.set(g);a.src=c;document.getElementsByTagName("head")[0].appendChild(a)})}var d=["loadApp"],e=this;
+function b(h){chrome.storage.local.get("ydn-crm-track",function(c){var a=c["ydn-crm-track"];c="jsc/ydn.crm-0.7.19.js";navigator.onLine&&"Edge"==a?c="https://ydn-src-1.storage.googleapis.com/jsc/ydn.crm-edge.js":"Beta"==a?c="jsc/ydn.crm-0.7.19.js":"RC"==a&&(c="jsc/ydn.crm-0.7.19.js");a=document.createElement("script");a.onload=h;a.type="text/javascript";var g={};g["ydn-crm-src"]=c;chrome.storage.local.set(g);a.src=c;document.getElementsByTagName("head")[0].appendChild(a)})}var d=["loadApp"],e=this;
 d[0]in e||!e.execScript||e.execScript("var "+d[0]);for(var f;d.length&&(f=d.shift());)d.length||void 0===b?e=e[f]?e[f]:e[f]={}:e[f]=b;})();
