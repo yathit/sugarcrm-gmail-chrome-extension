469f1a1ced24cea0c25b5ee067da73853d658586
Kyaw Tun <kyawtun@yathit.com>
2015-06-15 23:34:17 +0800

    new template.

--- js/environment.js
+++ js/environment.js
@@ -31,7 +31,7 @@ window.YathitCrm = {};
  * @enum {string} version number base on tracks.
  */
 YathitCrm.Version = {
-  release: '3.1.3'
+  release: '3.3.2'
 };
 
 
@@ -45,5 +45,5 @@ YathitCrm.sugarcrm = {};
  * @enum {string} version number base on tracks.
  */
 YathitCrm.sugarcrm.Version = {
-  release: '3.1.4'
+  release: '3.3.2'
 };
