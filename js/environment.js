035ab7e5bb07f12d3e3fe8a160c73cdfd44aa154
Kyaw Tun <kyawtun@yathit.com>
2015-07-12 08:20:58 +0800

    rel 3.4.6 b

--- js/environment.js
+++ js/environment.js
@@ -31,7 +31,7 @@ window.YathitCrm = {};
  * @enum {string} version number base on tracks.
  */
 YathitCrm.Version = {
-  release: '3.3.8'
+  release: '3.4.6'
 };
 
 
@@ -45,5 +45,5 @@ YathitCrm.sugarcrm = {};
  * @enum {string} version number base on tracks.
  */
 YathitCrm.sugarcrm.Version = {
-  release: '3.3.8'
+  release: '3.4.6'
 };
