71664e7102c02b4a63edd2e5270c7a088f2e5cf1
kyaw <kyawtun@yathit.com>
2016-04-27 19:50:04 +0800

    screen capture permission

--- js/environment.js
+++ js/environment.js
@@ -31,7 +31,7 @@ window.YathitCrm = {};
  * @enum {string} version number base on tracks.
  */
 YathitCrm.Version = {
-  release: '5.11.11'
+  release: '5.11.16'
 };
 
 
@@ -45,5 +45,5 @@ YathitCrm.sugarcrm = {};
  * @enum {string} version number base on tracks.
  */
 YathitCrm.sugarcrm.Version = {
-  release: '5.11.11'
+  release: '5.11.16'
 };
