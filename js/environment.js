c1b5d6de90f187897df3f6e7c3b370e7f73a1c66
kyaw <kyawtun@yathit.com>
2016-06-27 08:28:15 +0800

    version update.

--- js/environment.js
+++ js/environment.js
@@ -31,9 +31,9 @@ window.YathitCrm = {};
  * @enum {string} version number base on tracks.
  */
 YathitCrm.Version = {
-  release: '6.3.7',
-  beta: '6.3.8',
-  alpha: '6.3.8'
+  release: '6.3.11',
+  beta: '6.3.11',
+  alpha: '6.3.11'
 };
 
 
@@ -47,7 +47,7 @@ YathitCrm.sugarcrm = {};
  * @enum {string} version number base on tracks.
  */
 YathitCrm.sugarcrm.Version = {
-  release: '6.3.7',
-  beta: '6.3.8',
-  alpha: '6.3.8'
+  release: '6.3.14',
+  beta: '6.3.15',
+  alpha: '6.3.15'
 };
