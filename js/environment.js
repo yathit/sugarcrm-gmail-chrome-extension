ff0c7f4ae6f78ca4da3366723cc83d6f3ed3161a
mbikyaw <kyaw@mechanobio.info>
2019-06-24 09:20:16 +0800

    declearative injection

--- js/environment.js
+++ js/environment.js
@@ -33,9 +33,9 @@ window.YathitCrm = {};
  * @enum {string} version number base on tracks.
  */
 YathitCrm.Version = {
-  release: '11.0.0',
-  beta: '11.0.0',
-  alpha: '11.0.0'
+  release: '12.0.0',
+  beta: '12.0.0',
+  alpha: '12.0.0'
 };
 
 
@@ -49,9 +49,9 @@ YathitCrm.sugarcrm = {};
  * @enum {string} version number base on tracks.
  */
 YathitCrm.sugarcrm.Version = {
-  release: '11.0.3',
-  beta: '11.0.3',
-  alpha: '11.0.3'
+  release: '12.0.0',
+  beta: '12.0.0',
+  alpha: '12.0.0'
 };
 
 })(typeof window == 'undefined' ? self : window);
